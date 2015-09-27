100886a9b03de6e1b841f5138c98ed6ce7491590
James Jones <jamesjonesmath@gmail.com>
2015-09-26 21:12:34 -0500

    Create sort-rubric.user.js

---
+++ rubrics/sort-rubric.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Canvas Rubric Sort
+// @namespace   https://people.richland.edu/james
+// @description This program allows the user to sort rubric criteria
+// @include     https://*.instructure.com/courses/*/rubrics/*
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @version     2
+// @grant       none
+// ==/UserScript==
+waitForKeyElements('.rubric_container.rubric.editing', attachRowSorter);
+function attachRowSorter() {
+  $('.rubric_table tbody').sortable({
+    axis: 'y',
+    containment: 'parent',
+    items: '> tr'
+  });
+}
