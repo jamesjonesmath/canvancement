cca08651ce98f9a8e85ebe1ba599c119fd36e425
James Jones <jamesjonesmath@gmail.com>
2016-01-11 11:14:11 -0600

    Update and rename printed-links/hide-all-links.user.js to printed-urls/hide-all-urls.user.js

--- printed-urls/hide-all-urls.user.js
+++ printed-urls/hide-all-urls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==  
-// @name        No Print Links  
+// @name        Hide All URLs  
 // @namespace   https://github.com/jamesjonesmath/canvancement  
-// @description Hide the links when printing  
+// @description Hide the URL links when printing a page 
 // @include     https://*.instructure.com/courses/*/pages/*  
 // @version     1  
 // @grant       GM_addStyle  
