0814a408be3f52c35f995ef86339b45ad8072fd4
James Jones <jamesjonesmath@gmail.com>
2016-03-13 21:30:48 -0500

    Major rework, ready for release

--- users/admin/user-search-details.user.js
+++ users/admin/user-search-details.user.js
@@ -1,20 +1,26 @@
 // ==UserScript==
 // @name        Admin User Search Details
 // @namespace   https://github.com/jamesjonesmath/canvancement
-// @description Show the names of the students who have an assignment override
+// @description Show the Login ID and SIS User Id for users from the Admin Search Page
 // @include     /^https://.*\.instructure\.com/accounts/[0-9]+/users(\?|$)/
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 (function() {
   'use strict';
-  var css = '\
-    ul.users li.user a { display: inline-block; width: 15em; overflow: hidden; vertical-align: middle;} \
-    .login_id { display: inline-block; width: 8em; font-size: 90%; font-style: italic; padding-left: 2em; color: #666; overflow: hidden; vertical-align: middle;} \
-    .sis_user_id { display: inline-block; width 8em; text-align: right; font-size: 90%; padding-left: 1em; color: #666; overflow: hidden; vertical-align: middle;} \
-  ';
+  var config = {
+    'columns': [{
+      'field': 'login_id'
+    }, {
+      'field': 'sis_user_id',
+      'class': 'text-right'
+    }]
+  };
   var userRegex = new RegExp('/accounts/([0-9]+)/users/([0-9]+)');
   var userData = {};
+  $('ul.users').wrap('<div class="item-group-condensed"></div>');
+  $('ul.users').addClass('ig-list');
+  $('ul.users li.user a').wrap('<div class="ig-row"><div class="ig-row__layout"></div></div>').addClass('ig-title ellipses');
   $('ul.users li.user a').each(function(i, e) {
     var match = userRegex.exec(e.href);
     if (match) {
@@ -27,9 +33,6 @@
     $.getJSON(url, function(data, status, jqXHR) {
       var userId = data.id;
       userData[userId] = data;
-      if (Object.keys(userData).length == 1) {
-        $('head').append($('<style>').prop('type', 'text/css').html(css));
-      }
       updateInfo(data);
     });
   }
@@ -37,15 +40,20 @@
   function updateInfo(user) {
     var userId = user.id;
     var info = [];
-    if (typeof user.login_id !== 'undefined' && user.login_id) {
-      info.push('<span class="login_id">' + user.login_id + '</span>');
+    var i,
+      e,
+      col;
+    var details = $('<div>').addClass('ig-details');
+    for (i = 0; i < config.columns.length; i++) {
+      col = config.columns[i];
+      if (typeof user[col.field] !== 'undefined' && user[col.field]) {
+        e = $('<div>').addClass('ellipses').text(user[col.field]);
+        if (typeof col.class !== 'undefined' && col.class) {
+          e.addClass(col.class);
+        }
+        details.append(e);
+      }
     }
-    if (typeof user.sis_user_id !== 'undefined' && user.sis_user_id) {
-      info.push('<span class="sis_user_id">' + user.sis_user_id + '</span>');
-    }
-    if (info.length > 0) {
-      $('li#user_' + userId).append(info.join(' '));
-    }
+    $('li#user_' + userId + '> div > div').append(details);
   }
-
 })();
