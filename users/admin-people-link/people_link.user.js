a3c635ff01414947d91983bd8aef402bdfb9a6d8
James Jones <jamesjonesmath@gmail.com>
2016-08-29 23:03:23 -0500

    Create people_link.user.js

---
+++ users/admin-people-link/people_link.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name Admin People Link
+// @namespace https://github.com/jamesjonesmath/canvancement
+// @description Links to the course people page from the admin course search
+// @include /^https:\/\/.*\.instructure\.com\/accounts\/[0-9]+($|\/courses)/
+// @version 1
+// @grant none
+// ==/UserScript==
+(function() {
+  var courseIdRegex = new RegExp('^course_([0-9]+)$');
+  var items = document.querySelectorAll('div#content > ul.courses > li');
+  if (items) {
+    for (var i = 0; i < items.length; i++) {
+      var match = courseIdRegex.exec(items[i].id);
+      if (match) {
+        var courseId = match[1];
+        var span = items[i].querySelector('div.info > span.links');
+        if (span) {
+          var spacer = document.createTextNode(' | ');
+          span.appendChild(spacer);
+          var link = document.createElement('a');
+          link.href = '/courses/' + courseId + '/users';
+          link.textContent = 'People';
+          span.appendChild(link);
+        }
+      }
+    }
+  }
+})();
