b9667bd080dbac76bfb48b6c3cea7a3a20bad6be
James Jones <jamesjonesmath@gmail.com>
2016-09-11 20:06:42 -0500

    Create qw-rubrics.user.js

---
+++ quizzes/quizwiz/install/qw-rubrics.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        QuizWiz : Rubrics
+// @namespace   https://github.com/jamesjonesmath/canvancement
+// @description Regrading and speed enhancements for Canvas quizzes
+// @include     https://*.instructure.com/courses/*/gradebook/speed_grader?*
+// @include     https://*.instructure.com/courses/*/quizzes/*/history?*
+// @noframes
+// @version     2
+// @grant       none
+// ==/UserScript==
+requirejs([ 'https://cdn.gitcdn.link/repo/jamesjonesmath/canvancement/master/quizzes/quizwiz/src/qw-engine.js' ], function(QuizWiz) {
+  'use strict';
+
+  var config = {
+    // Regrading methods may be 'disabled', 'enabled', or 'autorun'
+    'methods' : {
+      'unanswered' : 'autorun',
+      'full_points' : 'disabled',
+      'ma_allnone' : 'disabled',
+      'ma_correct' : 'disabled',
+      'ma_difference' : 'disabled',
+      'fill_in_blanks' : 'disabled',
+      'dropdowns' : 'disabled'
+    },
+    // Speed enhancements may be true or false
+    'autoExpandComments' : true,
+    'duplicateQuestionHeader' : true,
+    'showButtonCounts' : true,
+    'nextAfterUpdate' : true,
+    'nextAfterComment' : true,
+    'nextAfterRubric' : true,
+    'nextRubricExpanded' : true
+  };
+
+  QuizWiz(config);
+});
