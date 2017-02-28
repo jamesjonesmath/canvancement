8f7cd83b8cbabb17f5c09ef672bb5613ce6957a8
James Jones <jamesjonesmath@gmail.com>
2017-02-28 01:11:12 -0600

    Do not run on iframes
    Was causing a problem with submitting a comment when a quiz was loaded in SpeedGrader because it was trying to add the style to the iframe contents as well as the SpeedGrader window.

--- miscellaneous/remove-max-width.user.js
+++ miscellaneous/remove-max-width.user.js
@@ -3,13 +3,16 @@
 // @namespace   https://github.com/jamesjonesmath/canvancement
 // @description Override CSS to lets pages expand to full width of page
 // @include     https://*.instructure.com/courses/*
-// @version     1
+// @include     https://*.instructure.com/accounts/*
+// @version     2
 // @grant       GM_addStyle
 // ==/UserScript==
 (function () {
   try {
-    GM_addStyle('body:not(.full-width) .ic-app-main-layout-vertical {max-width:none;}');
-    GM_addStyle('body:not(.full-width):not(.outcomes) .ic-Layout-wrapper {max-width:none;}');
+    // Do not run in iFrames
+    if (window === parent.window) {
+      GM_addStyle('body:not(.full-width):not(.outcomes) .ic-Layout-wrapper {max-width:none;}');
+    }
   } 
   catch (e) {
     console.log(e);
