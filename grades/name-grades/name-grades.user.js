06670b4b36b720afe9db4c85c58291b904756c44
James Jones <jamesjonesmath@gmail.com>
2018-08-01 21:37:44 -0500

    Major rewrite
    This removes the jQuery dependency and uses the window.fetch() command. This is supported by modern browsers, but not by Internet Explorer. In other words, this will not work with Internet Explorer.
    This changes the breadcrumb and its links
    This changes the document title that shows up on the printed page
    It now checks for the student grades table and works backwards to find a heading, rather than just looking for "Courses" in the heading. This keeps it from changing "Courses I'm Teaching" and works better with other languages.

--- grades/name-grades/name-grades.user.js
+++ grades/name-grades/name-grades.user.js
@@ -3,19 +3,33 @@
 // @namespace   https://github.com/jamesjonesmath/canvancement
 // @description Adds a users name to their all grades page
 // @include     https://*.instructure.com/users/*/grades
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
-var includeSisId = true;
-var nameOrder = [
-  'short_name',
-  'name',
-  'sortable_name'
-];
-var regex = new RegExp('/users/([0-9]+)/grades$');
-var matches = regex.exec(document.location);
-if (matches) {
-  $.getJSON('/api/v1/users/' + matches[1], function (data) {
+(function() {
+  'use strict';
+  var includeSisId = true;
+  var nameOrder = [ 'short_name', 'name', 'sortable_name' ];
+  var regex = new RegExp('/users/([0-9]+)/grades$');
+  var matches = regex.exec(document.location.pathname);
+  if (matches) {
+    var userId = matches[1];
+    var options = {
+      'credentials' : 'same-origin',
+      'headers' : {
+        'accept' : 'application/json'
+      }
+    };
+    fetch('/api/v1/users/' + userId, options)
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(updatePage)
+      .catch(function(error) {
+        console.log('Unable to lookup user information ', error);
+      });
+  }
+
+  function updatePage(data) {
     var name;
     for (var i = 0; i < nameOrder.length; i++) {
       var key = nameOrder[i];
@@ -24,12 +38,56 @@ if (matches) {
         break;
       }
     }
-    if (includeSisId && typeof data.sis_user_id !== 'undefined' && data.sis_user_id) {
-      name += ' (' + data.sis_user_id + ')';
-    }
-    console.log(name);
     if (typeof name !== 'undefined') {
-      $('h2:contains("Courses ")').text(name);
+      // Change the title on the page
+      var htmlTitle = document.title.split(':');
+      if (htmlTitle.length === 2) {
+        htmlTitle[1] = ' ' + name;
+        document.title = htmlTitle.join(':');
+      }
+      // Change the breadcrumb
+      var breadcrumb = document.querySelectorAll('nav#breadcrumbs ul li a');
+      if (breadcrumb.length === 3) {
+        var bcName = breadcrumb[1];
+        bcName.href = '/users/' + userId;
+        var bcNameText = bcName.querySelector('span');
+        if (bcNameText) {
+          bcNameText.textContent = name;
+        }
+        var bcGrades = breadcrumb[2];
+        bcGrades.href = '/users/' + userId + '/grades';
+      }
+      // Change the First heading, include SIS if requested
+      if (includeSisId && typeof data.sis_user_id !== 'undefined' && data.sis_user_id) {
+        name += ' (' + data.sis_user_id + ')';
+      }
+      // Start with table and work backwards to find heading
+      var table = document.querySelector('table.course_details.student_grades');
+      if (table) {
+        var heading = table.previousSibling;
+        while (heading && heading.nodeName !== 'H2') {
+          heading = heading.previousSibling;
+        }
+        if (heading) {
+          heading.textContent = name;
+        }
+      }
     }
-  });
-}
+  }
+
+  function parseJSON(response) {
+    return response.json();
+  }
+
+  function checkStatus(response) {
+    // From https://github.com/github/fetch
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      var error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+  }
+
+})();
