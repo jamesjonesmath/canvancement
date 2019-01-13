02704db7d0f578461d35225f6bbc6f741b37cfed
James Jones <jamesjonesmath@gmail.com>
2019-01-13 17:14:38 -0600

    Look for "essay" questions
    Canvas changed the class from short_answer_question to essay_question.
    Also added checking for answered questions and set the opacity to 0.5.

--- quizzes/grade_by_question/highlight_short_answer.user.js
+++ quizzes/grade_by_question/highlight_short_answer.user.js
@@ -3,52 +3,50 @@
 // @namespace   https://github.com/jamesjonesmath/canvancement
 // @description Highlights fill in the blank questions that are not correct in the one-at-a-time navigation links
 // @include     https://*.instructure.com/courses/*/quizzes/*/history?*
-// @version     1
+// @version     2
 // @grant none
 // ==/UserScript==
 (function() {
   'use strict';
-  if (/^\/courses\/[0-9]+\/quizzes\/[0-9]+\/history$/.test(window.location.pathname)) {
-    // Change your colors here. You can also use names like 'white' and 'black'
-    var backgroundColor = '#900';
-    var foregroundColor = '#eee';
-    highlightWrong(backgroundColor, foregroundColor);
+  if (!/^\/courses\/[0-9]+\/quizzes\/[0-9]+\/history$/.test(window.location.pathname)) {
+    return;
   }
+
+  // Change your colors here. You can also use names like 'white' and 'black'
+  var backgroundColor = '#900';
+  var foregroundColor = '#eee';
+  highlightWrong(backgroundColor, foregroundColor);
+
   function highlightWrong(bgColor, fgColor) {
-    var checkQuestionTypes = [ 'short_answer_question', 'fill_in_multiple_blanks_question' ];
-    try {
-      var nList = document.querySelectorAll('div#quiz-nav-inner-wrapper ul li');
-      if (nList.length === 0) {
-        return;
+    var checkQuestionTypes = [ 'essay_question', 'short_answer_question', 'fill_in_multiple_blanks_question' ];
+    var nList = document.querySelectorAll('div#quiz-nav-inner-wrapper ul li');
+    if (nList.length === 0) {
+      return;
+    }
+    var qList = document.querySelectorAll('div#questions div.question.display_question');
+    var qRegex = new RegExp('^question_([0-9]+)$');
+    for (var i = 0; i < qList.length; i++) {
+      var question = qList[i];
+      var match = qRegex.exec(question.id);
+      if (!match || question.classList.contains('correct')) {
+        continue;
       }
-      var qList = document.querySelectorAll('div#questions div.question.display_question');
-      var qRegex = new RegExp('^question_([0-9]+)$');
-      for (var i = 0; i < qList.length; i++) {
-        var match = qRegex.exec(qList[i].id);
-        if (match) {
-          if (qList[i].classList.contains('correct')) {
-            continue;
-          }
-          var questionId = match[1];
-          var navItem = document.getElementById('quiz_nav_' + questionId);
-          if (navItem) {
-            var process = false;
-            for (var j = 0; j < checkQuestionTypes.length; j++) {
-              if (qList[i].classList.contains(checkQuestionTypes[j])) {
-                process = true;
-                break;
-              }
-            }
-            if (process) {
-              var nItem = navItem.querySelector('a.question-nav-link');
-              nItem.style.backgroundColor = (typeof bgColor === 'undefined') ? 'black' : bgColor;
-              nItem.style.color = (typeof fgColor === 'undefined') ? 'white' : fgColor;
-            }
+      var questionId = match[1];
+      var navItem = document.getElementById('quiz_nav_' + questionId);
+      var qScore = question.querySelector('input.question_input_hidden[name="question_score_' + questionId + '"]');
+      if (navItem) {
+        var process = checkQuestionTypes.some(function(name) {
+          return question.classList.contains(name);
+        });
+        if (process) {
+          var nItem = navItem.querySelector('a.question-nav-link');
+          nItem.style.backgroundColor = (typeof bgColor === 'undefined') ? 'black' : bgColor;
+          nItem.style.color = (typeof fgColor === 'undefined') ? 'white' : fgColor;
+          if (qScore.value !== '') {
+            nItem.style.opacity = 0.5;
           }
         }
       }
-    } catch (e) {
-      console.log(e);
     }
   }
 })();
