8e46c4fef6e53cb5426f4427be7c263ae60c53b1
James Jones <jamesjonesmath@gmail.com>
2019-07-17 00:29:56 -0500

    Removed default CSS, supplied minimal CSS
    The default CSS supplied by TableSorter looked ugly with Canvas. This removes almost everything except for what is needed and a few touches for the spacing around the filter.
    
    The CSS can be removed if you remove the filter widget.

--- courses/all-courses/all-courses-sort.user.js
+++ courses/all-courses/all-courses-sort.user.js
@@ -4,26 +4,32 @@
 // @description Allows sorting on any column of the All Courses list
 // @include     https://*.instructure.com/courses
 // @require     https://cdn.jsdelivr.net/npm/tablesorter@2.31.1/dist/js/jquery.tablesorter.combined.min.js
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 (function() {
   'use strict';
   if (/^\/courses\/?$/.test(window.location.pathname)) {
-    const head = document.querySelector('head');
-    if (head) {
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.type = 'text/css';
-      link.href = 'https://cdn.jsdelivr.net/npm/tablesorter@2.31.1/dist/css/theme.default.min.css';
-      head.appendChild(link);
+    const el = document.createElement('style');
+    document.head.appendChild(el);
+    const styleSheet = el.sheet;
+    const css = [
+      '.tablesorter .filtered { display: none; }',
+      '.tablesorter-filter-row td { margin: 0; padding: 0 0.5rem; }',
+      'input.tablesorter-filter { margin: 0.25rem 0; padding: 0.2rem; width: 90%; }'
+    ];
+    for (let i = 0; i < css.length; i++) {
+      styleSheet.insertRule(css[i], i);
     }
     document.querySelectorAll('table.ic-Table th.course-list-star-column').forEach(function(e) {
       e.classList.add('filter-false');
     });
     $('table.ic-Table').tablesorter({
       'widgets' : [ 'filter' ],
-      // 'sortList' : [[3,1]]
+      // 'sortList' : [[3,1]],
+      'cssIconAsc' : 'icon-mini-arrow-up',
+      'cssIconDesc' : 'icon-mini-arrow-down',
+      'headerTemplate' : '{content}{icon}',
     });
   }
 })();
