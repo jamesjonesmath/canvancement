7cc21f2309c97c53f8f0323bf6a7f56f0f6b02eb
James Jones <jamesjonesmath@gmail.com>
2019-07-19 09:34:44 -0500

    Load tablesorter dynamically if not there
    Allow for inclusion in the global custom JavaScript by loading $.tablesorter if it's not already there.
    If you're running as a user script, then the user script manager loads it so it doesn't need to load tablesorter

--- courses/all-courses/all-courses-sort.user.js
+++ courses/all-courses/all-courses-sort.user.js
@@ -4,32 +4,39 @@
 // @description Allows sorting on any column of the All Courses list
 // @include     https://*.instructure.com/courses
 // @require     https://cdn.jsdelivr.net/npm/tablesorter@2.31.1/dist/js/jquery.tablesorter.combined.min.js
-// @version     2
+// @version     3
 // @grant       none
 // ==/UserScript==
 (function() {
   'use strict';
   if (/^\/courses\/?$/.test(window.location.pathname)) {
-    const el = document.createElement('style');
-    document.head.appendChild(el);
-    const styleSheet = el.sheet;
-    const css = [
-      '.tablesorter .filtered { display: none; }',
-      '.tablesorter-filter-row td { margin: 0; padding: 0 0.5rem; }',
-      'input.tablesorter-filter { margin: 0.25rem 0; padding: 0.2rem; width: 90%; }'
-    ];
-    for (let i = 0; i < css.length; i++) {
-      styleSheet.insertRule(css[i], i);
+    if (typeof $.tablesorter === 'object') {
+      sortAllCourses();
+    } else {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.jsdelivr.net/npm/tablesorter@2.31.1/dist/js/jquery.tablesorter.combined.min.js';
+      script.onload = sortAllCourses;
+      document.head.append(script);
     }
-    document.querySelectorAll('table.ic-Table th.course-list-star-column').forEach(function(e) {
-      e.classList.add('filter-false');
-    });
-    $('table.ic-Table').tablesorter({
-      'widgets' : [ 'filter' ],
-      // 'sortList' : [[3,1]],
-      'cssIconAsc' : 'icon-mini-arrow-up',
-      'cssIconDesc' : 'icon-mini-arrow-down',
-      'headerTemplate' : '{content}{icon}',
-    });
+    function sortAllCourses() {
+      const el = document.createElement('style');
+      document.head.appendChild(el);
+      const styleSheet = el.sheet;
+      const css = [ '.tablesorter .filtered { display: none; }', '.tablesorter-filter-row td { margin: 0; padding: 0 0.5rem; }',
+          'input.tablesorter-filter { margin: 0.25rem 0; padding: 0.2rem; width: 90%; }' ];
+      for (let i = 0; i < css.length; i++) {
+        styleSheet.insertRule(css[i], i);
+      }
+      document.querySelectorAll('table.ic-Table th.course-list-star-column').forEach(function(e) {
+        e.classList.add('filter-false');
+      });
+      $('table.ic-Table').tablesorter({
+        'widgets' : [ 'filter' ],
+        // 'sortList' : [[3,1]],
+        'cssIconAsc' : 'icon-mini-arrow-up',
+        'cssIconDesc' : 'icon-mini-arrow-down',
+        'headerTemplate' : '{content}{icon}',
+      });
+    }
   }
 })();
