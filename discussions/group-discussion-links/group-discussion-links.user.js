4ad5c0acf765f112a209aba2a53cdfe9b42535b0
James Jones <jamesjonesmath@gmail.com>
2019-10-30 00:18:05 -0500

    Remove jQuery dependency
    Canvas changed the loading of their JavaScripts and the userscripts execute before jQuery is available so the $.ajax() function wasn't working. Changed it to use the fetch() command that is part of modern browsers.
    Updated var to const or let.

--- discussions/group-discussion-links/group-discussion-links.user.js
+++ discussions/group-discussion-links/group-discussion-links.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/jamesjonesmath/canvancement
 // @description Create links from a group discussion that will return you back to the main course page or the discussion page
 // @include     https://*.instructure.com/groups/*/discussion_topics/*
-// @version     2
+// @version     3
 // @grant       none
 // ==/UserScript==
 (function() {
@@ -15,52 +15,55 @@
     // when switching to a new group, false will mimic the default Canvas
     // behavior of going to the main group page
 
-    var config = {
+    const config = {
       'show_names' : true,
       'switch_to_discussion' : true
     };
 
-    var pathRegex = new RegExp('^/groups/[0-9]+/discussion_topics/[0-9]+$');
+    const pathRegex = new RegExp('^/groups/[0-9]+/discussion_topics/[0-9]+$');
     if (!pathRegex.test(document.location.pathname)) {
       return;
     }
     if (typeof ENV.DISCUSSION === 'undefined' || typeof ENV.DISCUSSION.SEQUENCE === 'undefined') {
       return;
     }
-    var courseId = ENV.DISCUSSION.SEQUENCE.COURSE_ID;
-    var discussionId = ENV.DISCUSSION.SEQUENCE.ASSET_ID;
-    var infoElement = document.querySelector('div.discussion-subtitle ~ span.discussion-subtitle');
+    const courseId = ENV.DISCUSSION.SEQUENCE.COURSE_ID;
+    const discussionId = ENV.DISCUSSION.SEQUENCE.ASSET_ID;
+    const infoElement = document.querySelector('div.discussion-subtitle ~ span.discussion-subtitle');
     if (!infoElement) {
       return;
     }
-    var courseLink = infoElement.querySelector('a');
+    const courseLink = infoElement.querySelector('a');
     courseLink.textContent = 'Course';
     infoElement.appendChild(document.createTextNode(' > '));
-    var discussionLink = document.createElement('a');
-    discussionLink.href = '/courses/' + courseId + '/discussion_topics/' + discussionId;
+    const discussionLink = document.createElement('a');
+    discussionLink.href = `/courses/${courseId}/discussion_topics/${discussionId}`;
     discussionLink.textContent = 'Discussion';
     infoElement.appendChild(discussionLink);
     if (config.show_names) {
-      $.ajax({
-        'url' : '/api/v1/courses/' + courseId,
-        'dataType' : 'json',
-        'timeout' : 2000
-      }).done(function(data) {
-        var name = typeof data.course_code !== 'undefined' ? data.course_code : data.name;
-        courseLink.textContent = name;
+      const options = {
+        'credentials' : 'same-origin',
+        'headers' : {
+          'accept' : 'application/json'
+        }
+      };
+      fetch(`/api/v1/courses/${courseId}`, options).then(function(res) {
+        return res.json();
+      }).then(function(data) {
+        courseLink.textContent = typeof data.course_code !== 'undefined' ? data.course_code : data.name;
+        return Promise.resolve();
       });
-      $.ajax({
-        'url' : '/api/v1/courses/' + courseId + '/discussion_topics/' + discussionId,
-        'dataType' : 'json',
-        'timeout' : 2000
-      }).done(function(data) {
+      fetch(`/api/v1/courses/${courseId}/discussion_topics/${discussionId}`, options).then(function(res) {
+        return res.json();
+      }).then(function(data) {
         discussionLink.textContent = data.title;
+        return Promise.resolve();
       });
     }
     if (config.switch_to_discussion) {
-      var groupLinks = document.querySelectorAll('div#left-side > ul > li a');
-      if (groupLinks) {
-        for (var i = 0; i < groupLinks.length; i++) {
+      const groupLinks = document.querySelectorAll('div#left-side > ul > li a');
+      if (groupLinks.length) {
+        for (let i = 0; i < groupLinks.length; i++) {
           groupLinks[i].href = groupLinks[i].href + '/discussion_topics?root_discussion_topic_id=' + discussionId;
         }
       }
