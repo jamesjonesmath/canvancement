9323e02c8337ffc113bb569a008186c667c0d5b2
James Jones <jamesjonesmath@gmail.com>
2021-02-03 03:29:31 -0600

    Added throttling, code cleanup
    Added Bottleneck throttling library to keep from hitting the API so heavily. Defaults are 10 concurrent connections with a minimum delay between each one of 20 ms.
    Added a debug option to log the timings to the console for those who want to push the limits even more; default is false.
    Fixed a bug getting the next link
    Cleaned up code

--- assignments/intragroup-peer-reviews/intragroup-peer-reviews.user.js
+++ assignments/intragroup-peer-reviews/intragroup-peer-reviews.user.js
@@ -3,24 +3,38 @@
 // @description Assign intra-group peer reviews
 // @namespace   https://github.com/jamesjonesmath/canvancement
 // @match       https://*.instructure.com/courses/*/assignments/*/peer_reviews
-// @version     1
+// @require     https://cdn.jsdelivr.net/npm/bottleneck@2/light.min.js
+// @version     2
 // @grant       none
 // ==/UserScript==
+/* global Bottleneck */
 
 (function () {
   'use strict';
   const reloadPageWhenFinished = true;
+  const debug = false;
   let courseId = null;
   let assignmentId = null;
   let groupSets = null;
-  const pageRegex = new RegExp('^/courses/([0-9]+)/assignments/([0-9]+)/peer_reviews$');
+  const pageRegex = new RegExp(
+    '^/courses/([0-9]+)/assignments/([0-9]+)/peer_reviews$'
+  );
   const pageMatches = pageRegex.exec(window.location.pathname);
+
+  let limiter = null;
+  if (typeof Bottleneck === 'function') {
+    limiter = new Bottleneck({
+      maxConcurrent: 10,
+      minTime: 20,
+    });
+  }
+  const fetch = limiter ? limiter.wrap(window.fetch) : window.fetch;
 
   if (pageMatches) {
     courseId = pageMatches[1];
     assignmentId = pageMatches[2];
-    getJson('/api/v1/courses/' + courseId + '/group_categories?per_page=50')
-      .then(v => {
+    getJson(`/api/v1/courses/${courseId}/group_categories?per_page=50`).then(
+      v => {
         if (!v || v.length === 0) {
           return;
         }
@@ -31,23 +45,28 @@
           groupSets = sortEmbeddedNumeric(v, 'name');
           checkGroupAssignment();
         }
-      });
+      }
+    );
   }
 
   function checkGroupAssignment() {
-    getJson('/api/v1/courses/' + courseId + '/assignments/' + assignmentId)
-      .then(v => {
+    getJson(`/api/v1/courses/${courseId}/assignments/${assignmentId}`).then(
+      v => {
         addDialog(v.group_category_id);
-      });
+      }
+    );
   }
 
   function sortEmbeddedNumeric(data, keyField) {
     const numberRegex = new RegExp('([0-9]+)', 'g');
     for (let i = 0; i < data.length; i++) {
-      data[i].paddedKey = data[i][keyField].replace(numberRegex, (match, p1, offset, string) => {
-        const padded = '0000' + match;
-        return padded.substr(-5);
-      });
+      data[i].paddedKey = data[i][keyField].replace(
+        numberRegex,
+        (match, p1, offset, string) => {
+          const padded = '0000' + match;
+          return padded.substr(-5);
+        }
+      );
     }
     data.sort((a, b) => a.paddedKey.localeCompare(b.paddedKey));
     return data;
@@ -57,7 +76,9 @@
     if (typeof groupSets === 'undefined' || groupSets.length === 0) {
       return;
     }
-    const parent = document.querySelector('div#right-side-wrapper aside#right-side');
+    const parent = document.querySelector(
+      'div#right-side-wrapper aside#right-side'
+    );
     if (!parent) {
       return;
     }
@@ -67,12 +88,18 @@
     heading.textContent = 'Intra-Group Reviews';
     el.appendChild(heading);
     const intro = document.createElement('div');
-    intro.textContent = 'This will assign reviews to other people in the same group.' + (reloadPageWhenFinished ? ' If any peer reviews are assigned, this page will reload when finished.' : '');
+    intro.textContent =
+      'This will assign reviews to other people in the same group.' +
+      (reloadPageWhenFinished
+        ? ' If any peer reviews are assigned, this page will reload when finished.'
+        : '');
     el.appendChild(intro);
     const select = document.createElement('select');
     select.id = 'jj_intragroup_select';
     const defaultGroupSetId = assignmentGroupId ? assignmentGroupId : false;
-    select.add(new Option('Choose a Group Set', 0, defaultGroupSetId === false));
+    select.add(
+      new Option('Choose a Group Set', 0, defaultGroupSetId === false)
+    );
     for (let i = 0; i < groupSets.length; i++) {
       const item = groupSets[i];
       const groupSetId = item.id;
@@ -81,18 +108,20 @@
     }
     el.appendChild(select);
     const buttonDiv = document.createElement('div');
-    buttonDiv.classList.add('button-container', 'button-container-right-aligned');
+    buttonDiv.classList.add(
+      'button-container',
+      'button-container-right-aligned'
+    );
     const button = document.createElement('button');
     button.id = 'jj_intragroup_button';
     button.classList.add('btn');
     button.textContent = 'Assign IntraGroup Reviews';
     button.addEventListener('click', assignGroups, {
-      'once': true
+      once: true,
     });
     buttonDiv.appendChild(button);
     const progress = document.createElement('progress');
     progress.id = 'jj_intragroup_progress';
-    progress.textContent = '0%';
     progress.style.cssText = 'width: 90%; display: none; height: 2em;';
     buttonDiv.appendChild(progress);
     el.appendChild(buttonDiv);
@@ -110,61 +139,75 @@
     }
     const button = document.getElementById('jj_intragroup_button');
     button.style.display = 'none';
-    const progress = document.getElementById('jj_intragroup_progress');
-    progress.style.display = 'inline-block';
+    updateProgressBar();
     const groupSetId = selectedItems[0].value;
-    getJson('/api/v1/group_categories/' + groupSetId + '/groups?per_page=50')
-      .then(groupList => {
-        const groupIds = groupList.map(group => group.id);
-        Promise.all(groupIds.map(group => getJson('/api/v1/groups/' + group + '/users')))
-          .
-        then(values => {
-          const groups = [];
-          for (let i = 0; i < groupIds.length; i++) {
-            groups.push({
-              'id': groupIds[i],
-              'users': values[i].map(user => user.id),
-            });
-          }
-          addReviews(groups);
+    getJson(`/api/v1/group_categories/${groupSetId}/groups?per_page=50`).then(
+      processGroups
+    );
+  }
+
+  function processGroups(groupList) {
+    const groupIds = groupList.map(group => group.id);
+    const p = groupIds.map(groupId =>
+      getJson(`/api/v1/groups/${groupId}/users`)
+    );
+    Promise.all(p).then(values => {
+      const groups = [];
+      for (let i = 0; i < groupIds.length; i++) {
+        groups.push({
+          id: groupIds[i],
+          users: values[i].map(user => user.id),
         });
-      });
+      }
+      addReviews(groups);
+    });
   }
 
   function addReviews(groups) {
     const adds = computeNeeds(groups);
-    const progress = document.getElementById('jj_intragroup_progress');
-    let progressStatus = 0;
     if (adds.length) {
+      updateProgressBar(0);
       const n = adds.length;
-      progress.value = 0;
       let completed = 0;
-      for (let j = 0; j < adds.length; j++) {
-        let userId = adds[j].userId;
-        let csrfToken = getCookie('_csrf_token');
-        let url = '/courses/' + courseId + '/assignments/' + assignmentId + '/peer_reviews/users/' + userId;
-        let reviews = adds[j].reviews;
-        Promise.all(reviews.map(revieweeId => {
-            const data = {
-              'reviewee_id': revieweeId,
-              'authenticity_token': csrfToken
-            };
-            return postData(url, data);
-          }))
-          .then(function () {
-            completed++;
-            progressStatus = Math.round(100 * completed / n);
-            progress.value = progressStatus / 100;
-            progress.textContent = progressStatus + '%';
-            if (reloadPageWhenFinished && completed >= n) {
-              window.location.reload();
-            }
-          });
+      const csrfToken = getCookie('_csrf_token');
+      const pl = adds.map(add => {
+        const userId = add.userId;
+        const reviews = add.reviews;
+        const url = `/courses/${courseId}/assignments/${assignmentId}/peer_reviews/users/${userId}`;
+        const p = reviews.map(revieweeId => {
+          const data = {
+            reviewee_id: revieweeId,
+            authenticity_token: csrfToken,
+          };
+          return postData(url, data);
+        });
+        return Promise.all(p).then(() => {
+          completed++;
+          updateProgressBar(completed / n);
+          return Promise.resolve(true);
+        });
+      });
+      Promise.all(pl).then(() => {
+        updateProgressBar(1);
+        if (reloadPageWhenFinished && completed >= n) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+
+  function updateProgressBar(x) {
+    const progress = document.getElementById('jj_intragroup_progress');
+    if (progress) {
+      if (typeof x !== 'undefined') {
+        progress.value = x;
+        progress.textContent = `${Math.round(x * 100)}%`;
       }
-    } else {
-      progressStatus = 100;
-      progress.value = progressStatus / 100;
-      progress.textContent = progressStatus + '%';
+      if (typeof x === 'undefined' || x === 0) {
+        progress.style.display = 'inline-block';
+      } else if (x === 1) {
+        progress.style.display = 'none';
+      }
     }
   }
 
@@ -191,8 +234,8 @@
         }
         if (reviews.length) {
           adds.push({
-            'userId': userId,
-            'reviews': reviews
+            userId: userId,
+            reviews: reviews,
           });
         }
       }
@@ -202,7 +245,9 @@
 
   function getUsers() {
     const users = {};
-    const studentList = document.querySelectorAll('#content ul li.student_reviews');
+    const studentList = document.querySelectorAll(
+      '#content ul li.student_reviews'
+    );
     if (studentList.length > 0) {
       for (let i = 0; i < studentList.length; i++) {
         const item = studentList[i];
@@ -210,7 +255,9 @@
         if (userSpan) {
           const userId = userSpan.textContent;
           const reviews = [];
-          const assignedList = item.querySelectorAll('ul.peer_reviews li.peer_review.assigned');
+          const assignedList = item.querySelectorAll(
+            'ul.peer_reviews li.peer_review.assigned'
+          );
           if (assignedList) {
             for (let j = 0; j < assignedList.length; j++) {
               const assignSpan = assignedList[j].querySelector('span.user_id');
@@ -228,8 +275,7 @@
   }
 
   function getCookie(name) {
-    const cookies = document.cookie.split(';')
-      .map(cookie => cookie.trim());
+    const cookies = document.cookie.split(';').map(cookie => cookie.trim());
     let cookieValue = null;
     let i = 0;
     while (i < cookies.length && cookieValue === null) {
@@ -244,38 +290,39 @@
 
   function postData(url, data) {
     const init = {
-      'credentials': 'same-origin',
-      'headers': new Headers({
+      credentials: 'same-origin',
+      headers: new Headers({
         'content-type': 'application/json',
-        'accept': 'application/json'
+        'accept': 'application/json',
       }),
-      'method': 'POST',
-      'body': JSON.stringify(data)
+      method: 'POST',
+      body: JSON.stringify(data),
     };
     return fetch(url, init)
+      .then(res => displayLimits(res, url))
       .then(response => response.json());
   }
 
   function getJson(url) {
     const init = {
-      'credentials': 'same-origin',
-      'headers': new Headers({
+      credentials: 'same-origin',
+      headers: new Headers({
         'content-type': 'application/json',
-        'accept': 'application/json'
-      })
+        'accept': 'application/json',
+      }),
     };
     return fetch(url, init)
+      .then(res => displayLimits(res, url))
       .then(response => {
         const links = checkLinkHeader(response.headers.get('link'));
         if (typeof links !== 'undefined' && links.length) {
-          const promises = links.map(url => getJson(url));
+          const promises = links.map(u => getJson(u));
           promises.unshift(response.json());
-          return Promise.all(promises)
-            .then(values => {
-              const data = [];
-              values.map(v => Array.prototype.push.apply(data, v));
-              return data;
-            });
+          return Promise.all(promises).then(values => {
+            const data = [];
+            values.map(v => Array.prototype.push.apply(data, v));
+            return data;
+          });
         } else {
           return response.json();
         }
@@ -306,10 +353,18 @@
           }
         }
       } else {
-        urls.push(nextLink.toString());
+        urls.push(links.next.toString());
       }
     }
     return urls;
   }
 
+  function displayLimits(res, url) {
+    if (debug) {
+      const rate = res.headers.get('X-Rate-Limit-Remaining');
+      const runtime = res.headers.get('X-Runtime');
+      console.log(`${rate}\t${runtime}\t${res.status}\t${url}`);
+    }
+    return Promise.resolve(res);
+  }
 })();
