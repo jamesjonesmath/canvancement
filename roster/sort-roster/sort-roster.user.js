20181fbac272eefd05ad95e313a78f6d45dea0e2
James Jones <jamesjonesmath@gmail.com>
2021-12-03 01:22:46 -0600

    Support filtering data, internationlization
    Major rewrite and code cleanup
    Works when a user selects a filter
    Supports dates and times in other locales besides English
    Removed configuration section

--- roster/sort-roster/sort-roster.user.js
+++ roster/sort-roster/sort-roster.user.js
@@ -3,254 +3,339 @@
 // @namespace   https://github.com/jamesjonesmath/canvancement
 // @description Allows sorting on any column of the Canvas Course Roster
 // @include     https://*.instructure.com/courses/*/users
-// @require     https://cdn.jsdelivr.net/combine/npm/jquery@3.4.1/dist/jquery.slim.min.js,npm/tablesorter@2.31.1
-// @version     7
+// @require     https://cdn.jsdelivr.net/combine/npm/jquery@3.6.0,npm/tablesorter@2.31.1
+// @author      James Jones
+// @version     8
 // @grant       none
 // ==/UserScript==
-(function() {
+/* global ENV, jQuery */
+(function () {
   'use strict';
 
-  var pageRegex = new RegExp('^/courses/\\d+/users/?$');
+  const pageRegex = /^\/courses\/\d+\/users\/?$/;
   if (!pageRegex.test(window.location.pathname)) {
     return;
   }
 
-  var rosterColumns = {
-    'avatar' : {
-      'col' : 0,
-      'text' : 'Profile Picture'
-    },
-    'name' : {
-      'text' : 'Name',
-      'wrapper' : 'a',
-      'heading' : 'Name'
-    },
-    'login' : {
-      'text' : 'Login / SIS ID',
-      'heading' : 'Login/SIS'
-    },
-    'loginid' : {
-      'text' : 'Login ID',
-      'heading' : 'Login'
-    },
-    'sisid' : {
-      'text' : 'SIS ID'
-    },
-    'section' : {
-      'text' : 'Section',
-      'heading' : 'Section',
-      'wrapper' : 'div',
-      'shrink' : true
-    },
-    'role' : {
-      'text' : 'Role',
-      'wrapper' : 'div'
-    },
-    'last' : {
-      'text' : 'Last Activity',
-      'heading' : 'Last',
-      'wrapper' : 'div',
-      'tablesorter' : {
-        'sorter' : 'shortDateTime',
-        'empty' : 'bottom',
-        'sortInitialOrder' : 'desc'
-      }
-    },
-    'total' : {
-      'text' : 'Total Activity',
-      'heading' : 'Total',
-      'align' : 'right',
-      'tablesorter' : {
-        'sorter' : 'extendedTime',
-        'empty' : 'bottom',
-        'sortInitialOrder' : 'desc'
-      }
-    }
-  };
+  const months = generateMonths(ENV.LOCALE);
+  const dateRegex = generateDatePatterns();
 
   let jq = jQuery().jquery === '1.7.2' ? jQuery : jQuery.noConflict();
 
-  if (typeof jq.fn.tablesorter === 'undefined') {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/tablesorter@2.31.1/dist/js/jquery.tablesorter.combined.min.js';
-    script.onload = function() {
-      if (jQuery !== jq) {
-        jq = jQuery;
-      }
-      rosterSort();
-    };
-    document.head.appendChild(script);
-  }
-  else {
-    rosterSort();
+  checkJQuery();
+
+  function checkJQuery() {
+    if (typeof jq.fn.tablesorter === 'undefined') {
+      const script = document.createElement('script');
+      script.src =
+        'https://cdn.jsdelivr.net/npm/tablesorter@2.31.3/dist/js/jquery.tablesorter.combined.min.js';
+      script.onload = function () {
+        if (jQuery !== jq) {
+          jq = jQuery;
+        }
+        waitForRoster();
+      };
+      document.head.appendChild(script);
+    } else {
+      waitForRoster();
+    }
   }
 
-  function rosterSort(mutations, tableObserver) {
-    var roster = document.querySelector('table.roster tbody');
+  function waitForRoster(mutations, observer) {
+    const roster = document.querySelector('table.roster tbody');
     if (!roster) {
-      if (typeof tableObserver === 'undefined') {
-        var sel = document.getElementById('content');
-        var obs = new MutationObserver(rosterSort);
+      if (typeof observer === 'undefined') {
+        const sel = document.getElementById('content');
+        const obs = new MutationObserver(waitForRoster);
         obs.observe(sel, {
-          'childList' : true,
-          'subtree' : true
+          childList: true,
+          subtree: true,
         });
       }
       return;
     }
-    if (typeof tableObserver !== 'undefined') {
-      tableObserver.disconnect();
+    if (typeof observer !== 'undefined') {
+      observer.disconnect();
     }
-    try {
-      var tableColumns = document.querySelectorAll('table.roster thead tr th');
-      var sortHeaders = {};
-      var needParser = false;
-      var styles = {};
-      for (var c = 0; c < tableColumns.length; c++) {
-        var columnText = tableColumns[c].textContent.trim();
-        var match = false;
-        for ( var field in rosterColumns) {
-          if (rosterColumns.hasOwnProperty(field)) {
-            var info = rosterColumns[field];
-            if (typeof info.col !== 'undefined' && info.col == c) {
-              if (typeof info.heading === 'undefined') {
-                sortHeaders[c] = {
-                  'sorter' : false,
-                  'parser' : false
-                };
-                match = true;
-                break;
-              }
-            }
-            if (info.text == columnText) {
-              rosterColumns[field].col = c;
-              if (typeof info.align !== 'undefined') {
-                var nthchildSelector = ':nth-child(' + (1 + c) + ')';
-                styles['table.roster tr th' + nthchildSelector] = 'text-align:' + info.align + ';';
-                styles['table.roster tr td' + nthchildSelector] = 'text-align:' + info.align + ';';
-              }
-              if (typeof info.tablesorter !== 'undefined') {
-                sortHeaders[c] = info.tablesorter;
-                if (typeof info.tablesorter.sorter !== 'undefined') {
-                  needParser = true;
-                }
-              }
-              match = true;
-              break;
-            }
-          }
-        }
-        if (!match) {
-          sortHeaders[c] = {
-            'sorter' : false,
-            'parser' : false
-          };
-        }
+    if (roster) {
+      watchForUpdates();
+    }
+  }
+
+  function watchForUpdates(mutations, observer) {
+    if (typeof observer === 'undefined') {
+      const sel = document.querySelector('#content div[data-view="users"]');
+      const obs = new MutationObserver(watchForUpdates);
+      obs.observe(sel, { childList: true });
+      rosterSort();
+    } else {
+      if (
+        mutations.length === 1 &&
+        mutations[0].addedNodes.length > 0 &&
+        mutations[0].removedNodes.length > 0
+      ) {
+        rosterSort();
       }
-      addCSS(styles);
-      if (needParser) {
-        jq.tablesorter.addParser({
-          'id' : 'shortDateTime',
-          'format' : function(s, table, cell, cellIndex) {
-            var months = 'Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec';
-            var now = new Date();
-            var dateRegex = new RegExp('^(?:(' + months + ') (\\d+)|(\\d+) (' + months + '))(?:,? (\\d{4}))?');
-            var timeRegex = new RegExp('(\\d+):(\\d{2})(am|pm)?$');
-            var tm = '';
-            var monthStr, dayStr, month, day, hour, min;
-            var year = now.getFullYear();
-            if (dateRegex.test(s)) {
-              var dateMatch = dateRegex.exec(s);
-              if (typeof dateMatch[1] === 'undefined') {
-                dayStr = dateMatch[3];
-                monthStr = dateMatch[4];
-              } else {
-                monthStr = dateMatch[1];
-                dayStr = dateMatch[2];
-              }
-              month = months.indexOf(monthStr) / 4;
-              day = parseInt(dayStr);
-              if (typeof dateMatch[5] !== 'undefined') {
-                year = parseInt(dateMatch[5]);
-              }
-            } else {
-              month = now.getMonth();
-              day = now.getDay();
-            }
-            if (timeRegex.test(s)) {
-              var timeMatch = timeRegex.exec(s);
-              hour = parseInt(timeMatch[1]);
-              if (typeof timeMatch[3] !== 'undefined') {
-                if (hour == 12) {
-                  hour = 0;
-                }
-                if (timeMatch[3] == 'pm') {
-                  hour += 12;
-                }
-              }
-              min = parseInt(timeMatch[2]);
-            } else {
-              hour = 0;
-              min = 0;
-            }
-            tm = new Date(year, month, day, hour, min, 0).toISOString();
-            return tm;
-          },
-          'parsed' : false,
-          'type' : 'text'
-        });
-        jq.tablesorter.addParser({
-          'id' : 'extendedTime',
-          'format' : function(s, table, cell, cellIndex) {
-            var extendedTimeRegex = new RegExp('^(?:([0-9]+):)?([0-9]{2}):([0-9]{2})$', 'im');
-            var tm = '';
-            var matches = extendedTimeRegex.exec(s);
-            if (matches) {
-              var hrs = parseInt(matches[1]) || 0;
-              var mins = parseInt(matches[2]);
-              var secs = parseInt(matches[3]);
-              tm = 3600 * hrs + 60 * mins + secs;
-            }
-            return tm;
-          },
-          'parsed' : false,
-          'type' : 'numeric'
-        });
+    }
+  }
+
+  function rosterSort() {
+    const columns = scanColumns();
+    const tableColumns = document.querySelectorAll('table.roster thead tr th');
+    const sortHeaders = {};
+    const styles = {};
+    tableColumns.forEach((col, c) => {
+      if (col.children.length > 0) {
+        sortHeaders[c] = { sorter: false, parser: false };
+      } else if (c === columns.lastActivity || c === columns.totalActivity) {
+        sortHeaders[c] = {
+          sorter: c === columns.lastActivity ? 'shortDateTime' : 'extendedTime',
+          empty: 'bottom',
+          sortInitialOrder: 'desc',
+        };
+        const nthchildSelector = `:nth-child(${1 + c})`;
+        styles[`table.roster tr th${nthchildSelector}`] = `text-align: right;`;
+        styles[`table.roster tr td${nthchildSelector}`] = `text-align: right;`;
       }
-      var observerTarget = document.querySelector('table.roster tbody');
-      var rowsInRosterTable = observerTarget.rows.length;
-      if (rowsInRosterTable >= 50) {
-        var observer = new MutationObserver(function() {
-          if (observerTarget.rows.length != rowsInRosterTable) {
-            rowsInRosterTable = observerTarget.rows.length;
-            jq('table.roster.tablesorter').trigger('update', [ true ]);
-          }
-        });
-        observer.observe(observerTarget, {
-          'childList' : true
-        });
-      }
-      jq('table.roster').tablesorter({
-        'sortReset' : true,
-        'headers' : sortHeaders
+    });
+    addCSS(styles);
+    if (columns.lastActivity) {
+      jq.tablesorter.addParser({
+        id: 'shortDateTime',
+        format: shortDateTimeFormat,
+        parsed: false,
+        type: 'text',
       });
-    } catch (e) {
-      console.log(e);
     }
+    if (columns.totalActivity) {
+      jq.tablesorter.addParser({
+        id: 'extendedTime',
+        format: extendedTimeFormat,
+        parsed: false,
+        type: 'numeric',
+      });
+    }
+    watchForAdditionalRows();
+    jq('table.roster').tablesorter({
+      sortReset: true,
+      headers: sortHeaders,
+    });
+  }
+
+  function scanColumns() {
+    const cols = { lastActivity: null, totalActivity: null };
+    const rows = document.querySelectorAll('table.roster tbody tr.rosterUser');
+    if (rows.length === 0) {
+      return false;
+    }
+    const cells = rows[0].children;
+    for (let j = cells.length; j--; j > 0) {
+      const cell = cells[j - 1];
+      const div = cell.querySelector('div');
+      if (!div || div.classList.contains('admin-links')) {
+        continue;
+      }
+      if (div.classList.contains('section')) {
+        break;
+      }
+      if (
+        div.hasAttribute('data-tooltip') &&
+        div.hasAttribute('data-html-tooltip-title')
+      ) {
+        cols.lastActivity = j - 1;
+        cols.totalActivity = j;
+        break;
+      }
+    }
+    return cols;
+  }
+
+  function watchForAdditionalRows() {
+    const sel = document.querySelector('table.roster tbody');
+    let rowsInRosterTable = sel.rows.length;
+    if (rowsInRosterTable >= 50) {
+      const observer = new MutationObserver(function () {
+        if (sel.rows.length !== rowsInRosterTable) {
+          rowsInRosterTable = sel.rows.length;
+          jq('table.roster.tablesorter').trigger('update', [true]);
+        }
+      });
+      observer.observe(sel, {
+        childList: true,
+      });
+    }
+  }
+
+  function extractDateTooltip(s, cell) {
+    let txt = s;
+    if (cell.children.length === 1) {
+      const div = cell.children[0];
+      if (
+        div.hasAttribute('data-tooltip') &&
+        div.hasAttribute('data-html-tooltip-title')
+      ) {
+        txt = div.getAttribute('data-html-tooltip-title').trim();
+      }
+    }
+    return txt;
+  }
+
+  function shortDateTimeFormat(s, table, cell, cellIndex) {
+    if (s.trim() === '') {
+      return 0;
+    }
+    const timeRegex = /(?:(am|pm) )?(\d+)(?:[:.](\d{2}))(am|pm)?$/i;
+    let hour = 0;
+    let min = 0;
+    const t = extractDateTooltip(s, cell);
+    const dt = parseDate(s, t);
+    const timeMatch = timeRegex.exec(t);
+    if (timeMatch) {
+      const ampm = timeMatch[4] || timeMatch[1] || null;
+      hour = parseHour(timeMatch[2], ampm);
+      min = parseInt(timeMatch[3] || '00', 10);
+    }
+    return new Date(dt.y, dt.m, dt.d, hour, min, 0).toISOString();
+  }
+
+  function parseHour(hr = '0', ampm = null) {
+    let hour = parseInt(hr, 10);
+    if (ampm) {
+      if (hr === 12) {
+        hour = 0;
+      }
+      if (ampm.toLowerCase() === 'pm') {
+        hour += 12;
+      }
+    }
+    return hour;
+  }
+
+  function parseDate(dt = null, tooltip = null) {
+    const now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth();
+    let day = now.getDate();
+    const yearMatch = /\b(\d{4})\b/.exec(tooltip);
+    if (yearMatch) {
+      year = parseInt(yearMatch[1], 10);
+    }
+    for (const r of dateRegex) {
+      if (!r[0].test(dt)) {
+        continue;
+      }
+      const matches = r[0].exec(dt);
+      const seq = [...r[1]];
+      for (let i = 0; i < seq.length; i++) {
+        switch (seq[i]) {
+          case 'y':
+            year = parseInt(matches[i + 1], 10);
+            break;
+          case 'm':
+            month = lookupMonth(matches[i + 1]);
+            break;
+          case 'd':
+            day = parseInt(matches[i + 1], 10);
+            break;
+        }
+      }
+      break;
+    }
+    return { y: year, m: month, d: day };
+  }
+
+  function extendedTimeFormat(s, table, cell, cellIndex) {
+    const extendedTimeRegex = /^(?:(\d+):)?(\d{2}):(\d{2})$/im;
+    let tm = '';
+    const matches = extendedTimeRegex.exec(s);
+    if (matches) {
+      var hrs = parseInt(matches[1], 10) || 0;
+      var mins = parseInt(matches[2], 10);
+      var secs = parseInt(matches[3], 10);
+      tm = 3600 * hrs + 60 * mins + secs;
+    }
+    return tm;
   }
 
   function addCSS(styles) {
     if (typeof styles !== 'undefined' && Object.keys(styles).length > 0) {
-      var key, rule;
-      var style = document.createElement('style');
+      const style = document.createElement('style');
       document.head.appendChild(style);
-      var sheet = style.sheet;
-      var keys = Object.keys(styles);
-      for (var i = 0; i < keys.length; i++) {
-        key = keys[i];
-        rule = ' {' + styles[key] + '}';
+      const sheet = style.sheet;
+      const keys = Object.keys(styles);
+      for (const key of keys) {
+        const rule = ` {${styles[key]}`;
         sheet.insertRule(key + rule, sheet.cssRules.length);
       }
     }
+  }
+
+  function generateMonths(l = 'en') {
+    const locales = {
+      'ar': 'يناير|فبراير|مارس|أبريل|مايو|يونيو|يوليو|أغسطس|سبتمبر|أكتوبر|نوفمبر|ديسمبر',
+      'cy': 'Ion|Chwe|Maw|Ebr|Mai|Meh|Gor|Aws|Med|Hyd|Tach|Rhag',
+      'da': 'Jan|Feb|Mar|Apr|Maj|Jun|Jul|Aug|Sep|Okt|Nov|Dec',
+      'da-x-k12': 'Jan|Feb|Mar|Apr|Maj|Jun|Jul|Aug|Sep|Okt|Nov|Dec',
+      'de': 'Jan|Feb|März|Apr|Mai|Jun|Jul|Aug|Sept|Okt|Nov|Dez',
+      'el': 'Ιαν|Φεβ|Μαρ|Απρ|Μαϊ|Ιουν|Ιουλ|Αυγ|Σεπ|Οκτ|Νοε|Δεκ',
+      'en': 'Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec',
+      'en-AU': 'Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec',
+      'en-CA': 'Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec',
+      'en-GB': 'Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec',
+      'es': 'ene|feb|mar|abr|mayo|jun|jul|ago|sep|oct|nov|dic',
+      'fa': 'ژانویه|فوریه|مارس|آوریل|مه|ژوئن|ژوییه|آگوست|سپتامبر|اکتبر|نوامبر|دسامبر',
+      'fi': 'tammi|helmi|maalis|huhti|touko|kesä|heinä|elo|syys|loka|marras|joulu',
+      'fr': 'Janv|Févr|Mars|Avr|Mai|Juin|Juil|Août|Sept|Oct|Nov|Déc',
+      'fr-CA': 'Jan|Fév|Mar|Avr|Mai|Jun|Jul|Aou|Sep|Oct|Nov|Déc',
+      'he': 'ינו׳|פבר׳|מרץ|אפר׳|מאי|יונ|יול|אוג׳|ספט׳|אוק׳|נוב׳|דצמ׳',
+      'ht': 'jan|fev|mas|avr|me|jen|jiy|out|sep|okt|nov|des',
+      'hu': 'jan|feb|márc|ápr|máj|jún|júl|aug|szept|okt|nov|dec',
+      'hy': 'Հնվ|Փտր|Մրտ|Ապր|Մյս|Հնս|Հլս|Օգս|Սպտ|Հկտ|Նմբ|Դկտ',
+      'is': 'Jan|Feb|Mar|Apr|Maí|Jún|Júl|Ágú|Sep|Okt|Nóv|Des',
+      'it': 'gen|feb|mar|apr|mag|giu|lug|ago|set|ott|nov|dic',
+      'ja': '1月|2月|3月|4月|5月|6月|7月|8月|9月|10月|11月|12月',
+      'ko': '1월|2월|3월|4월|5월|6월|7월|8월|9월|10월|11월|12월',
+      'mi': 'Hān|Pēp|Māe|Āpe|Mei|Hun|Hūr|Āku|Hep|Oke|Noe|Tīh',
+      'nb': 'jan.|feb.|mars|april|mai|juni|juli|aug.|sep.|okt.|nov.|des.',
+      'nb-x-k12': 'jan.|feb.|mars|april|mai|juni|juli|aug.|sep.|okt.|nov.|des.',
+      'nl': 'jan|feb|mrt|apr|mei|jun|jul|aug|sep|okt|nov|dec',
+      'nn': 'jan|feb|mars|april|mai|juni|juli|aug|sep|okt|nov|des',
+      'pl': 'Sty|Lut|Mar|Kwi|Maj|Cze|Lip|Sie|Wrz|Paz|Lis|Gru',
+      'pt': 'jan|fev|mar|abr|mai|jun|jul|ago|set|out|nov|dez',
+      'pt-BR': 'jan|fev|mar|abr|mai|jun|jul|ago|set|out|nov|dez',
+      'ru': 'Янв|Фев|Мар|Апр|Май|Июн|Июл|Авг|Сен|Окт|Ноя|Дек',
+      'sl': 'jan|feb|mar|apr|maj|jun|jul|avg|sep|okt|nov|dec',
+      'sv': 'jan|feb|mar|apr|maj|jun|jul|aug|sep|okt|nov|dec',
+      'sv-x-k12': 'jan|feb|mar|apr|maj|jun|jul|aug|sep|okt|nov|dec',
+      'tr': 'Oca|Şub|Mar|Nis|May|Haz|Tem|Ağu|Eyl|Eki|Kas|Ara',
+      'uk': 'січ|лют|бер|квіт|трав|черв|лип|серп|вер|жовт|лист|груд',
+      'zh-Hans': '1月|2月|3月|4月|5月|6月|7月|8月|9月|10月|11月|12月',
+      'zh-Hant': '1月|2月|3月|4月|5月|6月|7月|8月|9月|10月|11月|12月',
+    };
+    const t = typeof locales[l] === 'string' ? locales[l] : locales.en;
+    return t.replace('.', '');
+  }
+
+  function lookupMonth(t) {
+    const mon = months.split('|').indexOf(t);
+    return mon > -1 ? mon : 0;
+  }
+
+  function generateDatePatterns(l = 'en') {
+    const datePatterns = ['M.*?D', 'D.*?M'];
+    if (/^1/.test(months)) {
+      datePatterns.push('m.*?D');
+    }
+    return datePatterns.map(e => {
+      const pattern = e
+        .replace(/([MDY])/gi, '!$1!')
+        .replace('!M!', `\\b(${months})\\b`)
+        .replace('!m!', `\\b(\\d+)\\b`)
+        .replace('!D!', '\\b(\\d+)\\b')
+        .replace('!Y!', '\\b(\\d{4})\\b');
+      const regex = new RegExp(`^${pattern}`, 'im');
+      const seq = e.replace(/[^MDY]/gi, '').toLowerCase();
+      return [regex, seq];
+    });
   }
 })();
