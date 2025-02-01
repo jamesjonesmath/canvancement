10975935ba75e239384d0613820e060e8fea4c5a
James Jones <jamesjonesmath@gmail.com>
2025-02-01 11:33:48 -0600

    New tweaks, code-upgrade
    * Hide unused navigation items that Canvas forces on you
    * Hide comment button on individual criteria
    * Hide URLS in printing
    * Remove forced 100% width for roster user name
    * Fix discussion split panel positioning to fixed instead of absolute
    * Upgraded code to be cleaner

--- miscellaneous/canvas-css-tweaks.user.js
+++ miscellaneous/canvas-css-tweaks.user.js
@@ -4,7 +4,7 @@
 // @description Various tweaks to Canvas CSS
 // @match       https://*.instructure.com/courses/*
 // @match       https://*.instructure.com/accounts/*
-// @version     5
+// @version     9
 // ==/UserScript==
 (function () {
   'use strict';
@@ -18,63 +18,140 @@
   const features = [
     'fullscreen', // use full width of page
     'sg_rubrics', // don't make rubrics so big in speedgrader
-    'sg_avatar_zoom', // zoom avatar in SpeedGrader
-    // 'sg_long_descriptions', // hide long descriptions in SpeedGrader rubrics
+    'sg_avatar_zoom', // zoom user avatar in SpeedGrader
+    'sg_long_descriptions', // hide long descriptions in SpeedGrader rubrics
     // 'sg_points_possible', // hide criterion points possible in SpeedGrader rubrics
-    // 'no_test' , // hide test instance notification in test and beta
+    'sg_criteria_comments', // hide criteria comments
+    //'no_test' , // hide test instance notification in test and beta
+    'hide_unused_course_navigation', // see forcedNavigationItems below
+    'hide_urls', // Do not print links
+    'roster_user_width', // Remove forced 100% width on roster user name,
+    'disc_split_panel_position', // Fix discussion split panel right panel scrolling
+  ];
+
+  // Canvas forces some navigation items even though you have them disabled.
+  // Items listed here will be hidden when they are disabled.
+  const forcedNavigationItems = [
+    'announcements',
+    'collaborations',
+    'conferences',
+    'outcomes',
+    // 'files',
+    // 'rubrics',
+    // 'pages',
   ];
 
   const rules = [];
 
   // Allow Canvas to use the full width of the page
-  if (features.indexOf('fullscreen') > -1) {
-    if (!document.body.classList.contains('full-width') && !document.body.classList.contains('outcomes')) {
-      rules.push('body:not(.full-width):not(.outcomes) #wrapper.ic-Layout-wrapper {max-width:none;}');
-    }
+  if (
+    features.includes('fullscreen') &&
+    !document.body.classList.contains('full-width') &&
+    !document.body.classList.contains('outcomes')
+  ) {
+    rules.push(
+      'body:not(.full-width):not(.outcomes) #wrapper.ic-Layout-wrapper {max-width:none;}'
+    );
   }
 
   // Override the min-width=60rem; that Canvas has on rubrics in speedgrader
-  if (features.indexOf('sg_rubrics') > -1) {
-    if (/^\/courses\/\d+\/gradebook\/speed_grader$/.test(window.location.pathname)) {
-      rules.push('#rubric_holder .rubric_container.rubric.assessing .react-rubric {min-width: min-content !important;}');
-      rules.push('#rubric_holder .rubric_container.rubric.assessing .react-rubric table {table-layout: auto;}');
-    }
+  if (features.includes('sg_rubrics') && isSpeedGrader()) {
+    rules.push(
+      '#rubric_holder .rubric_container.rubric.assessing .react-rubric {min-width:min-content!important;}'
+    );
+    rules.push(
+      '#rubric_holder .rubric_container.rubric.assessing .react-rubric table {table-layout:auto;}'
+    );
   }
 
   // Remove the long descriptions for rubrics and show only the titles
-  if (features.indexOf('sg_long_descriptions') > -1) {
-    if (/^\/courses\/\d+\/gradebook\/speed_grader$/.test(window.location.pathname)) {
-      rules.push('#rubric_holder .ratings .rating-description+span {display: none;}');
-    }
+  if (features.includes('sg_long_descriptions') && isSpeedGrader()) {
+    rules.push('#rubric_holder .rating-description+span {display:none;}');
+    rules.push('#rubric_holder .rating-footer {display:none;}');
   }
 
   // Hide the points possible for each criterion in SpeedGrader
-  if (features.indexOf('sg_points_possible') > -1) {
-    if (/^\/courses\/\d+\/gradebook\/speed_grader$/.test(window.location.pathname)) {
-      rules.push('#rubric_holder .criterion_points .graded-points > span > span + span {display: none;}');
-    }
+  if (features.includes('sg_points_possible') && isSpeedGrader()) {
+    rules.push(
+      '#rubric_holder td[data-testid="criterion-points"] .graded-points > span > span + span {display:none;}'
+    );
+  }
+
+  // Hide the individual criterion comments
+  if (features.includes('sg_criteria_comments') && isSpeedGrader()) {
+    rules.push(
+      '#rubric_holder td[data-testid="criterion-points"] > div > button {display:none;}'
+    );
+  }
+
+  // Zoom user's avatar in SpeedGrader upon mouseover
+  if (features.includes('sg_avatar_zoom') && isSpeedGrader()) {
+    rules.push('.gradebookAvatar:hover {transform: scale(2);}');
   }
 
   // Disable the test and beta instance banner at the bottom
-  if (features.indexOf('no_test') > -1) {
-    if (document.querySelector('#fixed_bottom .fixed_warning')) {
-      rules.push('#fixed_bottom .fixed_warning {display: none;}');
-    }
+  if (
+    features.includes('no_test') &&
+    document.querySelector('#fixed_bottom .fixed_warning')
+  ) {
+    rules.push('#fixed_bottom .fixed_warning {display:none;}');
   }
 
-  // Zoom avatar in SpeedGrader
-  if (features.indexOf('sg_avatar_zoom') > -1) {
-    if (/^\/courses\/\d+\/gradebook\/speed_grader$/.test(window.location.pathname)) {
-      rules.push('.gradebookAvatar:hover {transform: scale(2);}');
-    }
+  // Hide the unused course navigation links that Canvas forces on you but you know you won't use
+  if (
+    features.includes('hide_unused_course_navigation') &&
+    /^\/courses\/\d+/.test(window.location.pathname) &&
+    !document.body.classList.contains('full-width')
+  ) {
+    const items = document.querySelectorAll(
+      'ul#section-tabs li.section.section-hidden'
+    );
+    items.forEach(e => {
+      const classes = e.querySelector('a').classList;
+      const matching = forcedNavigationItems.filter(c => classes.contains(c));
+      if (matching.length > 0) {
+        e.style.display = 'none';
+      }
+    });
   }
 
-  if (rules.length) {
+  // Hide URLs when printing
+  if (features.includes('hide_urls')) {
+    rules.push('@media print {a:link:after, a:visited:after {content:none;}}');
+  }
+
+  // Remove the roster user width
+  if (
+    features.includes('roster_user_width') &&
+    /^\/courses\/\d+\/users\/?$/.test(window.location.pathname)
+  ) {
+    rules.push('td.roster_user_name-cell {width:inherit;}');
+  }
+
+  // Fix the positioning on the split discussion panel
+  if (
+    features.includes('disc_split_panel_position') &&
+    /^\/courses\/\d+\/discussion_topics\/\d+$/.test(window.location.pathname)
+  ) {
+    rules.push(
+      'div.discussion-redesign-layout div#DrawerLayoutTray {position:fixed;}'
+    );
+  }
+
+  if (rules.length > 0) {
     const style = document.createElement('style');
     document.head.appendChild(style);
     const sheet = style.sheet;
-    for (let i = 0; i < rules.length; i++) {
-      sheet.insertRule(rules[i],i);
+    let i = 0;
+    for (const rule of rules) {
+      sheet.insertRule(rule, i);
+      i++;
     }
   }
-}) ();
+
+  function isSpeedGrader() {
+    return /^\/courses\/\d+\/gradebook\/speed_grader$/.test(
+      window.location.pathname
+    );
+  }
+})();
